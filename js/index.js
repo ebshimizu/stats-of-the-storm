a7bba94937f8da9fd4e96c16f8264beaf6b82c12
Evan Shimizu <falindrith@gmail.com>
2018-01-23 02:00:17 -0500

    section transition and addition method added

--- js/index.js
+++ js/index.js
@@ -15,6 +15,7 @@ const ipcRenderer = require('electron').ipcRenderer
 const path = require('path');
 
 var DB;
+var sections = {};
 
 $(document).ready(initApp);
 var bgWindow;
@@ -53,12 +54,30 @@ function initGlobalUIHandlers() {
   // sidebar
   $('#main-menu').sidebar('setting', 'transition', 'overlay').
     sidebar('attach events', '#show-sidebar-button');
+
+  $('#main-menu .item').each(function(idx, elem) {
+    let sectionName = $(elem).attr('section-name');
+    $(elem).click(function() {
+      changeSection(sectionName);
+      $('#main-menu').sidebar('hide');
+    })
+  });
 }
 
 function loadSections() {
   // settings
   $('#main-content').append(getTemplate('settings', '#settings-page'));
   initSettingsPage();
+
+  $('#main-content').append(getTemplate('matches', '#matches-page'));
+  // initMatchesPage();
+
+  // register sections
+  sections.settings = {id: '#settings-page-content', title: 'Settings' };
+  sections.matches = {id: '#matches-page-content', title: 'Matches'};
+
+  // DEBUG: SHOWING SPECIFIC SECTION ON LOAD FOR TESTING
+  showSection('settings');
 }
 
 // returns the template contained in an import
@@ -74,4 +93,28 @@ function createBGWindow() {
   let bgPath = 'file://' + path.join(__dirname, './background.html');
   bgWindow = new BrowserWindow({width: 400, hegith: 400, show: false});
   bgWindow.loadURL(bgPath);
+}
+
+function changeSection(to) {
+  // this should only trigger for the visible section
+  for (let s in sections)
+    hideSection(s);
+
+  showSection(to);
+}
+
+function showSection(name) {
+  if ($(sections[name].id).hasClass('hidden'))
+    $(sections[name].id).transition('fade right');
+
+  setMenuTitle(sections[name].title);
+}
+
+function hideSection(name) {
+  if ($(sections[name].id).hasClass('visible'))
+    $(sections[name].id).transition('fade right');
+}
+
+function setMenuTitle(title) {
+  $('#section-menu-name').text(title);
 }
